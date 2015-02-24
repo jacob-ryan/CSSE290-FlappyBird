a2a52803ccfb059dbf1c3657376fd210ee687162
Peter Larson <larsonp@rose-hulman.edu>
2015-02-24 18:19:35 -0500

    Removed invalid characters from high score submission.
    This will prevent XSS. If we're going to let the class play this we
    probably want to do this.

--- submit-score.php
+++ submit-score.php
@@ -3,10 +3,11 @@
 	
 	if (isset($_GET["name"]) && isset($_GET["score"]) && isset($_GET["taps"]) && isset($_GET["games"]))
 	{
-		$name = $db->quote($_GET["name"]);
-		$score = $db->quote($_GET["score"]);
-		$taps = $db->quote($_GET["taps"]);
-		$games = $db->quote($_GET["games"]);
+		// This looks ugly, but it's a non-intrusive way to prevent XSS. It works because name should be alphanumeric, and the other three should only contain numbers. 
+		$name = $db->quote(preg_replace("/[^A-Za-z0-9 ]/", '',$_GET["name"]));
+		$score = $db->quote(preg_replace("/[^0-9 ]/", '',$_GET["score"]));
+		$taps = $db->quote(preg_replace("/[^0-9 ]/", '',$_GET["taps"]));
+		$games = $db->quote(preg_replace("/[^0-9 ]/", '',$_GET["games"]));
 		
 		$sql = "INSERT INTO Highscores VALUES(" . $name . ", " . $score . ", " . $taps . ", " . $games . ", UTC_TIMESTAMP());";
 		$db->query($sql);
