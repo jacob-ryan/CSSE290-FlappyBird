48cfdd398ed42a2f18b1851a466193fd6fc13466
Jacob Ryan <ryanjm@rose-hulman.edu>
2015-02-24 01:30:36 -0500

    Finished basic statistics page

--- statistics.php
+++ statistics.php
@@ -1,7 +1,7 @@
 <?php
 	include("common.php");
 ?>
-	<table>
+	<table class="statistics-table">
 		<tr>
 			<th>Max score:</th>
 			<td>
@@ -46,5 +46,90 @@
 	print($median);
 ?>
 			</td>
+		</tr>
+		<tr>
+			<th>Total score:</th>
+			<td>
+<?php
+	$sql = "SELECT SUM(score) AS scoreTotal FROM Highscores;";
+	$rows = $db->query($sql);
+	foreach ($rows as $row)
+	{
+		print($row["scoreTotal"]);
+	}
+?>
+			</td>
+		</tr>
+		<tr>
+			<th>Total taps:</th>
+			<td>
+<?php
+	$sql = "SELECT SUM(taps) AS tapsTotal FROM Highscores;";
+	$rows = $db->query($sql);
+	foreach ($rows as $row)
+	{
+		print($row["tapsTotal"]);
+	}
+?>
+			</td>
+		</tr>
+		<tr>
+			<th>Total games:</th>
+			<td>
+<?php
+	$sql = "SELECT SUM(games) AS gamesTotal FROM Highscores;";
+	$rows = $db->query($sql);
+	foreach ($rows as $row)
+	{
+		print($row["gamesTotal"]);
+	}
+?>
+			</td>
+		</tr>
+	</table>
+	<strong>Score breakdown:</strong>
+	<table class="distribution-graph">
+		<tr>
+<?php
+	$start = 0;
+	$end = 80;
+	$step = 10;
+	for ($i = $start; $i < $end; $i += $step)
+	{
+?>
+			<th><?= $i ?> - <?= $i + $step - 1 ?>:</th>
+<?php
+	}
+?>
+		</tr>
+		<tr>
+<?php
+	$sql = "SELECT COUNT(*) AS total FROM Highscores;";
+	$rows = $db->query($sql);
+	foreach ($rows as $row)
+	{
+		$total = $row["total"];
+	}
+	$counts = array();
+	for ($i = $start; $i < $end; $i += $step)
+	{
+		$counts[] = 0;
+	}
+	$sql = "SELECT COUNT(*) AS count, FLOOR(score / 10) AS score FROM Highscores GROUP BY FLOOR(score / 10);";
+	$rows = $db->query($sql);
+	foreach ($rows as $row)
+	{
+		$counts[$row["score"]] = $row["count"];
+	}
+	for ($i = $start; $i < $end; $i += $step)
+	{
+		$height = $counts[$i / 10] / $total * 300;
+?>
+			<td>
+				<div style="height: <?= $height ?>px;"><?= $counts[$i / 10] ?></div>
+			</td>
+<?php
+	}
+?>
 		</tr>
 	</table>
